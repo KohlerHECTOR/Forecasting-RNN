e12e0703d74c8e0ef65babc390bfdd53f9c30e91
hectorKOHLER <>
2021-11-11 17:19:58 +0100

    sequence forecast

--- exo3.py
+++ exo3.py
@@ -1,56 +1,48 @@
-from utils import RNN, device,SampleMetroDataset, testing
+from utils import RNN, device,ForecastMetroDataset, testing_forecast
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 torch.cuda.empty_cache()
-# Nombre de stations utilisé
-CLASSES = 10
-#Longueur des séquences
+
+CLASSES = 80 #max
 LENGTH = 20
-# Dimension de l'entrée (1 (in) ou 2 (in/out))
 DIM_INPUT = 2
-#Taille du batch
-BATCH_SIZE = 18
-
+HORIZON = None
+BATCH_SIZE = 1
 DIM_LATENT = 16
-
-LR = 1e-4
-
+LR = 6e-4
 PATH = "data/"
 
-
 matrix_train, matrix_test = torch.load(open(PATH+"hzdataset.pch","rb"))
-ds_train = SampleMetroDataset(matrix_train[:, :, :CLASSES, :DIM_INPUT], length=LENGTH)
-ds_test=SampleMetroDataset(matrix_test[:, :, :CLASSES, :DIM_INPUT], length = LENGTH, stations_max = ds_train.stations_max)
+ds_train = ForecastMetroDataset(matrix_train[:, :, :CLASSES, :DIM_INPUT], length=LENGTH) # x = st_1 : st_length - 1 ; y = st_2 : st_length
+ds_test=ForecastMetroDataset(matrix_test[:, :, :CLASSES, :DIM_INPUT], length = LENGTH, stations_max = ds_train.stations_max)
 data_train = DataLoader(ds_train,batch_size=BATCH_SIZE,shuffle=True)
 data_test = DataLoader(ds_test, batch_size=BATCH_SIZE,shuffle=False)
 
-#  TODO:  Question 2 : prédiction de la ville correspondant à une séquence
-writer = SummaryWriter("runs/latent_dim_"+str(DIM_LATENT))
+writer = SummaryWriter("runs/forecast/latent_dim_"+str(DIM_LATENT))
 
-
-
-rnn = RNN(BATCH_SIZE, dim_in = DIM_INPUT, dim_latent = DIM_LATENT, dim_out = CLASSES)
+rnn = RNN(batch_size = CLASSES, dim_in = DIM_INPUT, dim_latent = DIM_LATENT, dim_out = DIM_INPUT)
 optimizer = torch.optim.Adam(rnn.parameters())
-CEL = torch.nn.CrossEntropyLoss()
-CEL = CEL.to(device)
 
+MSE = torch.nn.MSELoss()
+MSE = MSE.to(device)
 
 # Train
 epoch = 40 # 20 epochs, batch de 18 dure environe 4 mins et score de 53 %
 for n_iter in range(epoch):
     loss_all_batches = 0
     for x,y in data_train:
+        x = x.reshape((CLASSES, LENGTH - 1, DIM_INPUT))
+        y = y.reshape((CLASSES, LENGTH - 1, DIM_INPUT))
         try:
-            assert x.size(0) == BATCH_SIZE # last batch is of size 4
+            assert x.size(0) == CLASSES # last batch is of size 4
             rnn.forward(x)
             # MANY TO ONE ARCHITECTURE
-            yhat = rnn.decode(rnn.hidden_states[-1])
+            yhat = rnn.decode(rnn.hidden_states[1:])
+            yhat = yhat.reshape((CLASSES, LENGTH - 1, DIM_INPUT))
             y = y.to(device)
-            loss = CEL.forward(yhat, y)
+            loss = MSE.forward(yhat, y)
             loss_all_batches += loss
-
-
 
             optimizer.zero_grad()
             loss.backward()
@@ -62,4 +54,4 @@ for n_iter in range(epoch):
 
     writer.add_scalar('Loss/train', loss_all_batches, n_iter)
     print(f"Epoch {n_iter}: loss {loss_all_batches}")
-    testing(data_test, BATCH_SIZE, n_iter, rnn, writer, CEL)
+    testing_forecast(data_test, BATCH_SIZE, n_iter, rnn, writer, MSE, HYPERPARAMS = (CLASSES, LENGTH, DIM_INPUT))
